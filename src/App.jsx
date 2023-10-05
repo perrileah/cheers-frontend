ecf1b5367bd5159379e5aba3a4cde033d3eb1851
Leah Perri <perrileah2023@gmail.com>
2023-10-05 15:23:35 -0500

    renders map within react app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { BrowserRouter } from "react-router-dom";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = "pk.eyJ1IjoibGVhaHBlcnJpIiwiYSI6ImNsaDB3cXN4aDBhaTQzcHVqdzJhb3hiZDMifQ.UjViRKqejimhTNWFCTH9iA";
 
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
   return (
     <div>
       <BrowserRouter>
         <Header />
+        <div ref={mapContainer} className="map-container" />
         <Content />
         <Footer />
       </BrowserRouter>
