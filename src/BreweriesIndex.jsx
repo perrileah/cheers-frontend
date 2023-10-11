400fd99405d29d7d3494e551db3768c14adf8f69
Leah Perri <perrileah2023@gmail.com>
2023-10-11 13:54:08 -0500

    adds basic search bar to brewery index

--- src/BreweriesIndex.jsx
+++ src/BreweriesIndex.jsx
@@ -1,26 +1,44 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function BreweriesIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+  console.log(props);
   return (
-    <div>
+    <div id="breweries-index">
+      <input
+        placeholder="Find a Brewery!"
+        type="text"
+        value={searchFilter}
+        onChange={(event) => setSearchFilter(event.target.value)}
+        list="breweries"
+      />
+      <datalist id="breweries">
+        {props.breweries.map((brewery) => (
+          <option key={brewery.id}>{brewery.name}</option>
+        ))}
+      </datalist>
+
       <h1>All Breweries</h1>
-      {props.breweries.map((brewery) => (
-        <div className="brewery-list" key={brewery.id}>
-          <Link to={`/breweries/${brewery.id}`}>
-            <h2>{brewery.name}</h2>
-            <img src={brewery.image_url} alt="image link" />
-            <p>
-              <strong>Address: </strong>
-              {brewery.address + "., " + brewery.city + ", " + brewery.state + " " + brewery.zip}
-            </p>
-            <p>
-              <strong>Website: </strong>
-              {brewery.website_url}
-            </p>
-            <button onClick={() => props.onShowBrewery(brewery)}>More info</button>
-          </Link>
-        </div>
-      ))}
+      {props.breweries
+        .filter((brewery) => brewery.name.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((brewery) => (
+          <div key={brewery.id}>
+            <Link to={`/breweries/${brewery.id}`}>
+              <h2>{brewery.name}</h2>
+              <img src={brewery.image_url} alt="image link" />
+              <p>
+                <strong>Address: </strong>
+                {brewery.address + "., " + brewery.city + ", " + brewery.state + " " + brewery.zip}
+              </p>
+              <p>
+                <strong>Website: </strong>
+                {brewery.website_url}
+              </p>
+              <button onClick={() => props.onShowBrewery(brewery)}>More info</button>
+            </Link>
+          </div>
+        ))}
     </div>
   );
 }
