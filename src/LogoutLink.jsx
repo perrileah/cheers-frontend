2da9b8550a62dc8c85eab08fdd01e8f0e425fcbf
Leah Perri <perrileah2023@gmail.com>
2023-11-11 13:15:49 -0600

    tries to troubleshoot conditional rendering in nav bar

--- src/LogoutLink.jsx
+++ src/LogoutLink.jsx
@@ -9,7 +9,7 @@ export function LogoutLink() {
   };
 
   return (
-    <a href="#" onClick={handleClick}>
+    <a class="logout" href="#" onClick={handleClick}>
       Logout
     </a>
   );
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -22,7 +22,7 @@ export function Signup() {
   };
 
   return (
-    <div id="signup">
+    <div id="signup" class="login">
       <h1>Signup</h1>
       <ul>
         {errors.map((error) => (
